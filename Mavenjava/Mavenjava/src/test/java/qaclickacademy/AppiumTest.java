16cc70a45037561959a9d7ae25f6e1d16340b441
sravani <shravanikasturi49@gmail.com>
2022-02-17 10:58:11 +0530

    first commit

---
+++ Mavenjava/Mavenjava/src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void nativeApp() {
+		System.out.println("nativeApp");
+	}
+
+	@Test
+	public void iosApps() {
+		System.out.println("iosapps");
+	}
+}
---
+++ Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void postJira() {
+		System.out.println("postjira");
+
+	}
+
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deletetwitter");
+
+	}
+
+}
